dbf8ffe73eb5934c3f4a7e3f29f8e7a236357440
João Paulo Sousa <joao.sousafatec@gmail.com>
2019-06-22 21:13:25 -0300

    traduzindo comentarios default

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,20 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // Esse widget é a raiz da sua aplicacao.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
+        // Este é o tema da sua aplicacao
         //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        // Tente rodar sua apalicacao com "flutter run". Voce vai ver a
+        // aplicacao com o toolbar azul. Entao, sem fechar o app, tente
+        // mudar a primarySwatch abaixo para Colors.green e entao chame o
+        // "hot reload" (pressione "r" no seu console quando voce roda "flutter run",
+        // ou simplesmente salve suas mudancas para o "hot reload" funcionar na sua IDE, automaticamente).
+        // Observe que o contador não voltou para zero; a aplicacao nao reiniciou.
         primarySwatch: Colors.pink,
       ),
       home: MyHomePage(title: 'Toia Chata'),
@@ -29,14 +28,14 @@ class MyHomePage extends StatefulWidget {
 
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  // Este widget eh a pagina inicial da sua aplicacao. Ele é stateful, ou seja,
+  // o objeto possui estado (definido abaixo) que contem campos que afetam
+  // sua aparencia
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  // Esta classe é a configuracao para o estado. Ele contém os valores (neste caso
+  // o titulo) fornecido pelo seu pai (neste caso, o widget app) e
+  // usado pelo metodo para construir o estado do metodo. Os campos no widget da subclasse
+  // sempre sao marcados pelo "final"
 
   final String title;
 
@@ -50,47 +49,46 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+      // Esta chamada para setState diz para o framework Flutter que alguma coisa tem
+      // alterado o seu estado, que faz com que execute novamente o metodo
+      // de construcao abaixo, para que a exibicao possa refletir os valores atualizados.
+      // Se mudar o _counter sem chamar setState(), o metodo de compilacao nao seria
+      // chamado novamente, e entao nada parece acontecer.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
+    // Este metodo é rodado novamente toda vez que setState for chamado, por exemplo como foi feito
+    // pelo metodo _incrementCounter acima.
     //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // O framework Flutter foi bem otimizado para executar metodo novamente
+    // bem rápido, para que voce possa reconstruir qualquer coisa que precise ser atualizada,
+    // em vez de precisar alterar individualmente instancias de widget  .
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        // Aqui nos levamos o valor para o objeto MyHomePage que foi criado pelo
+        // metodo App.build, e usado para setar o titulo do appbar.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+        // Center é um layout do widget. Leva um unico child e o
+        // posiciona no meio do parent, nesse caso o child abaixo.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
+          // Column vai além de um layout widget. Ele leva a lista de childrens e
+          // organzia de forma vertical. Por padrao, o tamanho em si para caber o seu
+          // children horizontalmente, e tenta ser tao alto quanto seu parent.
           //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
+          // Chame "debug painting" (pressione "p" no console, escolha a
+          // acao "Toggle Debug Paint" para o Flutter Inspector no Android
+          // Studio, ou o comando "Toggle Debug Paint" no Visual Studio Code)
+          // para ver o wireframe para cada widget.
           //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+          // Column possui varias propriedades para controlar o tamanho e
+          // a posicao do children. Aqui é usado o mainAxisAlignment para
+          // centralizar o children verticalmente; o eixo principal aqui é o eixo vertical
+          // porque a coluna é vertical (o eixo central seria horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -107,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Incrementar',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), // Essa virgula final torna a formatacao automática mais agrádavel para os métodos de criacao
     );
   }
 }
